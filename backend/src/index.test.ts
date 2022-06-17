e37a3a7ccca080dba00f0aaff694df28c2b7a99e
Eze Anyanwu <ezeanyinabia.anyanwu@gmail.com>
2022-06-16 23:55:32 -0400

    Rust -> NodeJS :) (#10)

---
+++ backend/src/index.test.ts
@@ -0,0 +1,7 @@
+import assert from "assert";
+
+describe("Test module", function () {
+  it("should work", function () {
+    assert.equal(1, 1);
+  });
+});
--- wtmm/src/hooks.rs
+++
@@ -1,5 +0,0 @@
-use rusqlite::hooks::Action;
-
-pub fn update(action: Action, db_name: &str, table_name: &str, row_id: i64) {
-    println!("{:?},{:?},{:?},{:?}", action, db_name, table_name, row_id);
-}
