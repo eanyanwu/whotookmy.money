785e967cc76bedaa8ed6562dff1201f49809481e
eanyanwu <ezeanyinabia.anyanwu@gmail.com>
2022-06-27 18:16:23 -0400

    lint

--- src/wtmm_server.ts
+++ src/wtmm_server.ts
@@ -45,7 +45,10 @@ export const createWtmmServer = (
       statusCode: 302,
       headers: {
         Location: "/dashboard",
-        "Set-Cookie": [`id=${user.userId};Max-Age=2147483647`, `mac=${mac};Max-Age=2147483647`],
+        "Set-Cookie": [
+          `id=${user.userId};Max-Age=2147483647`,
+          `mac=${mac};Max-Age=2147483647`,
+        ],
       },
     };
   });
