e97ca0b8531c080ba2f14f01f145ba70541c6324
eanyanwu <ezeanyinabia.anyanwu@gmail.com>
2022-07-05 09:43:10 -0400

    Edge case

--- src/data/index.ts
+++ src/data/index.ts
@@ -391,6 +391,12 @@ export const dailySpend = (user: User, period: number): DailySpend[] => {
         timeseries[tsIdx].purchases.push(purchase);
         timeseries[tsIdx].spend += purchase.amountInCents;
       } else {
+        // TODO: actially figure this edge case out. 
+        // getRecentPurchases sometimes returns purchases before the start time 
+        if (purchaseTimestamp < start.toUnixInteger()) {
+          purchaseIdx += 1;
+          continue;
+        }
         tsIdx += 1;
       }
     }
